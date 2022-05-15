284046ac93a6da5108974c7754103df672a310fc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-15 23:55:55 +0900

    #33 Form login custom 5

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -20,8 +20,23 @@ class Login extends Component {
         return (
             <div className="login-background">
                 <div className="login-container">
-                    <div className="login-content">
-                        abc
+                    <div className="login-content row">
+                        <div className="col-12 text-center">Login</div>
+                        <div className="col-12 form-group">
+                            <label>Username:</label>
+                            <input type="text" className="form-control" />
+                        </div>
+                        <div className="col-12 form-group">
+                            <label>Password:</label>
+                            <input type="password" className="form-control" />
+                        </div>
+                        <button>Login</button>
+                        <div className="col-12">
+                            <span>Forgot your password?</span>
+                        </div>
+                        <div className="col-12">
+
+                        </div>
                     </div>
                 </div>
             </div>
