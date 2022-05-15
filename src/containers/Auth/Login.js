1b0dcdd45bf32f85660a04ee6bb65c368d950b9c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-15 23:19:54 +0900

    #33 just check

---
+++ src/containers/Auth/Login.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { push } from "connected-react-router";
+
+import * as actions from "../store/actions";
+import './Login.scss';
+import { FormattedMessage } from 'react-intl';
+
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+
+    render() {
+
+        return (
+            <div>Hello login</div>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        language: state.app.language
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        navigate: (path) => dispatch(push(path)),
+        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
+        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
