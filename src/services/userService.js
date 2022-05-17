e3707815d70d32c97a7435faa11f22a956479aba
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-17 23:20:47 +0900

    #36 Login React + Node 5

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,5 +1,10 @@
 import axios from '../axios'
-export const handleLogin = (email, password) => {
+
+const handleLogin = (email, password) => {
     //goi den server Nodejs
     return axios.post('/api/login')
+}
+
+module.exports = {
+    handleLogin: handleLogin,
 }
