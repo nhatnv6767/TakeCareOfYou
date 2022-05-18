1e55bb9977e012d353a08de28d06ed605dd0f227
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-18 22:11:58 +0900

    #36 Login React + Node 7

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -7,7 +7,7 @@ import * as actions from "../../store/actions";
 
 import './Login.scss';
 import { FormattedMessage } from 'react-intl';
-import { userService } from '../../services'
+import userService from '../../services/userService'
 
 
 class Login extends Component {
