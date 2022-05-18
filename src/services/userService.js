2f79048efbcdece3d5cebfbcf5efaf928e2cebed
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-18 22:51:05 +0900

    #36 Login React + Node Some bug 10

--- src/services/userService.js
+++ src/services/userService.js
@@ -2,7 +2,7 @@ import axios from '../axios'
 
 const handleLoginApi = (email, password) => {
     //goi den server Nodejs
-    return axios.post('/api/login')
+    return axios.post('/api/login', { email, password })
 }
 
 export { handleLoginApi }
