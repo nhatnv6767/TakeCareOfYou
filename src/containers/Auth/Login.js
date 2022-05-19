de517a2470988b69b32349bc6902b488dd8a2527
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-19 20:32:03 +0900

    #36 Login React + Node 25

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -143,8 +143,7 @@ const mapStateToProps = state => {
 const mapDispatchToProps = dispatch => {
     return {
         navigate: (path) => dispatch(push(path)),
-        adminLoginSuccess: (adminInfo) => dispatch(actions.adminLoginSuccess(adminInfo)),
-        adminLoginFail: () => dispatch(actions.adminLoginFail()),
+        // userLoginFail: () => dispatch(actions.userLoginFail()),
         userLoginSuccess: (userInfor) => dispatch(actions.userLoginSuccess(userInfor)),
     };
 };
