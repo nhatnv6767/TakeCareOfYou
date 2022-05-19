523a2c50532c0df671f35505f573632b89f57036
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-19 20:34:48 +0900

    #36 Login React + Node 27

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -8,3 +8,11 @@ export const userLoginSuccess = (userInfo) => ({
     type: actionTypes.USER_LOGIN_SUCCESS,
     userInfo: userInfo
 })
+
+export const userLoginFail = () => ({
+    type: actionTypes.USER_LOGIN_FAIL
+})
+
+export const processLogout = () => ({
+    type: actionTypes.PROCESS_LOGOUT
+})
