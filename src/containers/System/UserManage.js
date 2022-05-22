f2cdcf1345662345a7c67366c9c3e867e0f89ee7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-22 14:14:57 +0900

    #40 CRUD with ReactJS - add new user 6

--- src/containers/System/UserManage.js
+++ src/containers/System/UserManage.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 import './UserManage.scss'
 import { getAllUsers } from '../../services/userService'
+import ModalUser from './ModalUser';
 
 class UserManage extends Component {
 
@@ -44,6 +45,7 @@ class UserManage extends Component {
         let arrUsers = this.state.arrUsers
         return (
             <div className="users-container">
+                <ModalUser />
                 <div className="title text-center">Manage users</div>
                 <div className="mx-1">
                     <button
