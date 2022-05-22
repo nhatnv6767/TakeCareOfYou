4d5c760f6f8230881f6be785447d7730bd8a7f4c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-22 14:57:49 +0900

    #40 CRUD with ReactJS - add new user 10

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -24,7 +24,7 @@ class ModalUser extends Component {
         console.log('Check child props', this.props)
         console.log('Check child open modal', this.props.isOpen)
         return (
-            <Modal isOpen={true} toggle={() => { this.toggle() }} className="abcClassName">
+            <Modal isOpen={this.props.isOpen} toggle={() => { this.toggle() }} className="abcClassName">
                 <ModalHeader toggle={() => { this.toggle() }}>Create a new user</ModalHeader>
                 <ModalBody>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
