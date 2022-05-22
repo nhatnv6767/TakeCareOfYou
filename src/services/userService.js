20de5765054364f51a09c13dbd683f9b0530d3f1
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-22 22:01:17 +0900

    #41 CRUD with ReactJS 15

--- src/services/userService.js
+++ src/services/userService.js
@@ -9,4 +9,9 @@ const getAllUsers = (inputId) => {
     return axios.get(`/api/get-all-users?id=${inputId}`)
 }
 
-export { handleLoginApi, getAllUsers }
+const createNewUser = (data) => {
+    console.log('Check data from service: ', data)
+    return axios.post('/api/create-new-user', data)
+}
+
+export { handleLoginApi, getAllUsers, createNewUser }
