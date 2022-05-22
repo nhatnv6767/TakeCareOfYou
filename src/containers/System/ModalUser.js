912f9cc1ae3494b087ff397b6df2145471021282
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-22 14:03:24 +0900

    #40 CRUD with ReactJS - add new user 4

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -1,15 +1,46 @@
 import { ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
+import React, { Component } from 'react';
+import { FormattedMessage } from 'react-intl';
+import { connect } from 'react-redux';
+class ModalUser extends Component {
+
+    state = {
+
+    }
+
+    componentDidMount() {
+    }
+
+
+    render() {
+        return (
+            <Modal isOpen={modal} toggle={toggle} className="className">
+                <ModalHeader>Modal title</ModalHeader>
+                <ModalBody>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={toggle}>Calcel</Button>
+                </ModalFooter>
+            </Modal>
+        )
+    }
+
+}
+
+const mapStateToProps = state => {
+    return {
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ModalUser);
 
 
 
-<Modal isOpen={modal} toggle={toggle} className="className">
-    <ModalHeader>Modal title</ModalHeader>
-    <ModalBody>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </ModalBody>
-    <ModalFooter>
-        <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-        <Button color="secondary" onClick={toggle}>Calcel</Button>
-    </ModalFooter>
-</Modal>
