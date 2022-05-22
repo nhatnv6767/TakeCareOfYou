0b9c68fe4bc313dc9cede5d04d55ad71f41e9727
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-22 15:24:37 +0900

    #40 CRUD with ReactJS - add new user 13 SCSS

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -24,10 +24,25 @@ class ModalUser extends Component {
         console.log('Check child props', this.props)
         console.log('Check child open modal', this.props.isOpen)
         return (
-            <Modal isOpen={this.props.isOpen} toggle={() => { this.toggle() }} className="abcClassName">
+            <Modal
+                isOpen={this.props.isOpen}
+                toggle={() => { this.toggle() }}
+                className="modal-user-container"
+                size="lg"
+            // centered
+            >
                 <ModalHeader toggle={() => { this.toggle() }}>Create a new user</ModalHeader>
                 <ModalBody>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+                    <div className="input-container">
+                        <label>Email</label>
+                        <input type="text" />
+                    </div>
+                    <div className="input-container">
+                        <label>Password</label>
+                        <input type="password" />
+                    </div>
+
                 </ModalBody>
                 <ModalFooter>
                     <Button color="primary" onClick={() => { this.toggle() }}>Do Something</Button>{' '}
