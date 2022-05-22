f5db34096c705a505cb3e5df2f515de7af4cb7a9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-22 14:12:27 +0900

    #40 CRUD with ReactJS - add new user 5

--- src/containers/System/ModalUser.js
+++ src/containers/System/ModalUser.js
@@ -1,28 +1,35 @@
-import { ModalBody, ModalFooter, ModalHeader } from "reactstrap";
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from "reactstrap";
 
 import React, { Component } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
 class ModalUser extends Component {
 
-    state = {
+    constructor(props) {
+        super(props);
+        this.state = {
 
+        }
     }
 
     componentDidMount() {
     }
 
+    toggle = () => {
+
+    }
+
 
     render() {
         return (
-            <Modal isOpen={modal} toggle={toggle} className="className">
-                <ModalHeader>Modal title</ModalHeader>
+            <Modal isOpen={true} toggle={() => { this.toggle() }} className="abcClassName">
+                <ModalHeader toggle={() => { this.toggle() }}>Modal title</ModalHeader>
                 <ModalBody>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </ModalBody>
                 <ModalFooter>
-                    <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-                    <Button color="secondary" onClick={toggle}>Calcel</Button>
+                    <Button color="primary" onClick={() => { this.toggle() }}>Do Something</Button>{' '}
+                    <Button color="secondary" onClick={() => { this.toggle() }}>Calcel</Button>
                 </ModalFooter>
             </Modal>
         )
