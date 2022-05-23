e2e487e8a6bb93b8488e5dba8ae4674df2d2340e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-23 22:22:07 +0900

    #44 Design interface 4.2

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "./HomeHeader.scss";
 
 class HomeHeader extends Component {
   render() {
-    return <div>Home Page -- HomeHeader</div>;
+    return <div className="home-header-container"></div>;
   }
 }
 
