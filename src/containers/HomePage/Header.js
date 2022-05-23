8382bd001f30686981544b0ce88b5081e75032bd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-23 22:14:30 +0900

    #44 Design interface 4

---
+++ src/containers/HomePage/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+class Header extends Component {
+  render() {
+    return <div>Home Page -- Header</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
