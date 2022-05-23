45455d8365e4566c2c241616be211006e9b91a8b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-24 00:24:52 +0900

    #44 Design interface 12

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -16,28 +16,28 @@ class HomeHeader extends Component {
               <div>
                 <b>Chuyên khoa</b>
               </div>
-              <div>Tìm bác sĩ theo chuyên khoa</div>
+              <div className="subs-title">Tìm bác sĩ theo chuyên khoa</div>
             </div>
 
             <div className="child-content">
               <div>
                 <b>Cơ sở y tế</b>
               </div>
-              <div>Chọn bệnh viện phòng khám</div>
+              <div className="subs-title">Chọn bệnh viện phòng khám</div>
             </div>
 
             <div className="child-content">
               <div>
                 <b>Bác sĩ</b>
               </div>
-              <div>Chọn bác sĩ giỏi</div>
+              <div className="subs-title">Chọn bác sĩ giỏi</div>
             </div>
 
             <div className="child-content">
               <div>
                 <b>Gói khám</b>
               </div>
-              <div>Khám sức khoẻ tổng quát</div>
+              <div className="subs-title">Khám sức khoẻ tổng quát</div>
             </div>
           </div>
           <div className="right-content"></div>
