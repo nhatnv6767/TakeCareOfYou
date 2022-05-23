2e7c1f365dd577046a834bf187f4d45e93881e0a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-24 00:18:43 +0900

    #44 Design interface 11

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -11,7 +11,35 @@ class HomeHeader extends Component {
             <i className="fas fa-bars"></i>
             <div className="header-logo"></div>
           </div>
-          <div className="center-content"></div>
+          <div className="center-content">
+            <div className="child-content">
+              <div>
+                <b>Chuyên khoa</b>
+              </div>
+              <div>Tìm bác sĩ theo chuyên khoa</div>
+            </div>
+
+            <div className="child-content">
+              <div>
+                <b>Cơ sở y tế</b>
+              </div>
+              <div>Chọn bệnh viện phòng khám</div>
+            </div>
+
+            <div className="child-content">
+              <div>
+                <b>Bác sĩ</b>
+              </div>
+              <div>Chọn bác sĩ giỏi</div>
+            </div>
+
+            <div className="child-content">
+              <div>
+                <b>Gói khám</b>
+              </div>
+              <div>Khám sức khoẻ tổng quát</div>
+            </div>
+          </div>
           <div className="right-content"></div>
         </div>
       </div>
