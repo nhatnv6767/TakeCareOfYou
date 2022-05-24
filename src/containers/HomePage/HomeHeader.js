53e0d0e15c66d800c0c1e6cfe0c0eca4cd664f9a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-25 00:53:57 +0900

    #47 Redux => Change language 1

--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 import "./HomeHeader.scss";
 import logo from "../../assets/logo.svg";
 import { FormattedMessage } from "react-intl";
+import { LANGUAGES } from "../../utils";
 
 class HomeHeader extends Component {
   render() {
@@ -64,8 +65,16 @@ class HomeHeader extends Component {
                 <i className="fas fa-question-circle"></i>
                 <FormattedMessage id="homeheader.support" />
               </div>
-              <div className="language-vi">VN</div>
-              <div className="language-en">EN</div>
+              <div className="language-vi">
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                  VI
+                </span>
+              </div>
+              <div className="language-en">
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                  EN
+                </span>
+              </div>
             </div>
           </div>
         </div>
