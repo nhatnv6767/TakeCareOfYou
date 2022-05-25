1424674175e22ebbc2cf86637649fbff99bf0618
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-26 04:15:17 +0900

    #48 React Slider Carousel 1 npm install 2

---
+++ src/containers/HomePage/Section/Specialty.js
@@ -0,0 +1,181 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import "./Specialty.scss";
+import { FormattedMessage } from "react-intl";
+
+class Specialty extends Component {
+  render() {
+    // props.language => lấy từ trong redux ra
+    let language = this.props.language;
+    return (
+      <React.Fragment>
+        <div className="home-header-container">
+          <div className="home-header-content">
+            <div className="left-content">
+              <i className="fas fa-bars"></i>
+              <img className="header-logo" src={logo} />
+            </div>
+            <div className="center-content">
+              <div className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="Specialty.speciality" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="Specialty.searchdoctor" />
+                </div>
+              </div>
+
+              <div className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="Specialty.health-facility" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="Specialty.select-room" />
+                </div>
+              </div>
+
+              <div className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="Specialty.doctor" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="Specialty.select-doctor" />
+                </div>
+              </div>
+
+              <div className="child-content">
+                <div>
+                  <b>
+                    <FormattedMessage id="Specialty.fee" />
+                  </b>
+                </div>
+                <div className="subs-title">
+                  <FormattedMessage id="Specialty.check-health" />
+                </div>
+              </div>
+            </div>
+            <div className="right-content">
+              <div className="support">
+                <i className="fas fa-question-circle"></i>
+                <FormattedMessage id="Specialty.support" />
+              </div>
+              <div
+                className={
+                  language === LANGUAGES.VI
+                    ? "language-vi active"
+                    : "language-vi"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.VI)}>
+                  VI
+                </span>
+              </div>
+              <div
+                className={
+                  language === LANGUAGES.EN
+                    ? "language-en active"
+                    : "language-en"
+                }
+              >
+                <span onClick={() => this.changeLanguage(LANGUAGES.EN)}>
+                  EN
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="home-header-banner">
+          <div className="content-up">
+            <div className="title1">
+              <FormattedMessage id="banner.title1" />
+            </div>
+            <div className="title2">
+              <FormattedMessage id="banner.title2" />
+            </div>
+            <div className="search">
+              <i className="fas fa-search"></i>
+              <input type="text" placeholder="Tìm chuyên khoa khám bệnh" />
+            </div>
+          </div>
+          <div className="content-down">
+            <div className="options">
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="far fa-hospital"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child1" />
+                </div>
+              </div>
+
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="fas fa-mobile-alt"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child2" />
+                </div>
+              </div>
+
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="fas fa-procedures"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child3" />
+                </div>
+              </div>
+
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="fas fa-flask"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child4" />
+                </div>
+              </div>
+
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="fas fa-user-md"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child5" />
+                </div>
+              </div>
+
+              <div className="option-child">
+                <div className="icon-child">
+                  <i className="fas fa-briefcase-medical"></i>
+                </div>
+                <div className="text-child">
+                  <FormattedMessage id="banner.child6" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
