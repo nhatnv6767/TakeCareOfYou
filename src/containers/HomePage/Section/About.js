2d1cf52675622cdd36f298bfa96c92ff6ce09a55
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-28 18:04:23 +0900

    #51 Design interface 6

--- src/containers/HomePage/Section/About.js
+++ src/containers/HomePage/Section/About.js
@@ -4,7 +4,17 @@ import { FormattedMessage } from "react-intl";
 
 class About extends Component {
   render() {
-    return <div className="section-share section-about">About Us</div>;
+    return (
+      <div className="section-share section-about">
+        <div className="section-about-header">
+          Truyền thông nói về Booking Care
+        </div>
+        <div className="section-about-content">
+          <div className="content-left"></div>
+          <div className="content-right"></div>
+        </div>
+      </div>
+    );
   }
 }
 
