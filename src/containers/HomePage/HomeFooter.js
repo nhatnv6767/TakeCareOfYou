6c764006f8ad38793e8e4c0d759a1942acdbb43c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-28 17:46:39 +0900

    #51 Design interface 3

---
+++ src/containers/HomePage/HomeFooter.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+
+class HomeFooter extends Component {
+  render() {
+    return <div className="section-share section-about">About Us</div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeFooter);
