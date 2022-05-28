d8af2b3495a3495d90d1b3a3b6d3991acc3ca27d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-28 15:33:15 +0900

    #50 Finish design 8.3

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -16,27 +16,27 @@ class Specialty extends Component {
           <div className="section-body">
             <Slider {...this.props.settings}>
               <div className="specialty-customize">
-                <div className="bg-image" />
+                <div className="bg-image section-specialty" />
                 <div>Cơ xương khớp 1</div>
               </div>
               <div className="specialty-customize">
-                <div className="bg-image" />
+                <div className="bg-image section-specialty" />
                 <div>Cơ xương khớp 2</div>
               </div>
               <div className="specialty-customize">
-                <div className="bg-image" />
+                <div className="bg-image section-specialty" />
                 <div>Cơ xương khớp 3</div>
               </div>
               <div className="specialty-customize">
-                <div className="bg-image" />
+                <div className="bg-image section-specialty" />
                 <div>Cơ xương khớp 4</div>
               </div>
               <div className="specialty-customize">
-                <div className="bg-image" />
+                <div className="bg-image section-specialty" />
                 <div>Cơ xương khớp 5</div>
               </div>
               <div className="specialty-customize">
-                <div className="bg-image" />
+                <div className="bg-image section-specialty" />
                 <div>Cơ xương khớp 6</div>
               </div>
             </Slider>
