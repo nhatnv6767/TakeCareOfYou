c1f768b8d36b2bddba371fee26dc8b1c1e52ab98
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-29 00:38:39 +0900

    #55 Menu accessible 7

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -4,19 +4,24 @@ export const adminMenu = [
     name: "menu.admin.manage-user",
     menus: [
       {
-        name: "menu.system.system-administrator.header",
-        subMenus: [
-          {
-            name: "menu.system.system-administrator.user-manage",
-            link: "/system/user-manage",
-          },
-          {
-            name: "menu.system.system-administrator.user-redux",
-            link: "/system/user-redux",
-          },
-        ],
+        name: "menu.admin.manage-doctor",
+        link: "/system/",
+        // subMenus: [
+        //   {
+        //     name: "menu.system.system-administrator.user-manage",
+        //     link: "/system/user-manage",
+        //   },
+        //   {
+        //     name: "menu.system.system-administrator.user-redux",
+        //     link: "/system/user-redux",
+        //   },
+        // ],
       },
-      // { name: 'menu.system.system-parameter.header', link: '/system/system-parameter' },
+
+      {
+        name: "menu.admin.manage-admin",
+        link: "/system/",
+      },
     ],
   },
 ];
