b93ac52d3b76091e59f4dc4e66460c3d3ecc5235
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-28 17:43:11 +0900

    #51 Design interface

---
+++ src/containers/HomePage/Section/About.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
+import Slider from "react-slick";
+
+class About extends Component {
+  render() {
+    return <div className="section-share section-about"></div>;
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(About);
