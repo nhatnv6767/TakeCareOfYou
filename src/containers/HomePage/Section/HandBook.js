77a58d5f6ebeb193b79304fadf25ade4dabed556
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-28 16:28:42 +0900

    #50 Finish design 11.2

--- src/containers/HomePage/Section/HandBook.js
+++ src/containers/HomePage/Section/HandBook.js
@@ -6,7 +6,7 @@ import Slider from "react-slick";
 class HandBook extends Component {
   render() {
     return (
-      <div className="section-share section-specialty">
+      <div className="section-share section-handbook">
         <div className="section-container">
           <div className="section-header">
             <span className="title-section">Cẩm nang</span>
@@ -15,27 +15,27 @@ class HandBook extends Component {
           <div className="section-body">
             <Slider {...this.props.settings}>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
+                <div className="bg-image section-handbook" />
                 <div>Cơ xương khớp 1</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
+                <div className="bg-image section-handbook" />
                 <div>Cơ xương khớp 2</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
+                <div className="bg-image section-handbook" />
                 <div>Cơ xương khớp 3</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
+                <div className="bg-image section-handbook" />
                 <div>Cơ xương khớp 4</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
+                <div className="bg-image section-handbook" />
                 <div>Cơ xương khớp 5</div>
               </div>
               <div className="section-customize">
-                <div className="bg-image section-specialty" />
+                <div className="bg-image section-handbook" />
                 <div>Cơ xương khớp 6</div>
               </div>
             </Slider>
