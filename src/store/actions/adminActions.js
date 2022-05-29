a7c738f38923744ac7309cbc167568c006327e7e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-29 22:19:03 +0900

    #57 Redux bonus 5.5

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -1,8 +1,18 @@
 import actionTypes from "./actionTypes";
+import { getAllCodeService } from "../../services/userService";
 
-export const fetchGenderStart = () => ({
-  type: actionTypes.FETCH_GENDER_START,
-});
+// export const fetchGenderStart = () => ({
+//   type: actionTypes.FETCH_GENDER_START,
+// });
+
+export const fetchGenderStart = async (dispatch, getState) => {
+  try {
+    let res = await getAllCodeService;
+  } catch (e) {
+    fetchGenderFailed();
+    console.log("FetchGenderStart error: ", e);
+  }
+};
 
 export const fetchGenderSuccess = () => ({
   type: actionTypes.FETCH_GENDER_SUCCESS,
