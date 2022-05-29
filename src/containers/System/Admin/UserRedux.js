d791f9c8bddc8e1a79877cd21afdd3d83636f069
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-29 18:44:15 +0900

    #56 Design Responsive Form 3

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -16,78 +16,22 @@ class UserRedux extends Component {
         <div className="user-redux-body">
           <div className="container">
             <div className="row">
-              <form>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input
-                      type="email"
-                      class="form-control"
-                      id="inputEmail4"
-                      placeholder="Email"
-                    />
-                  </div>
-                  <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input
-                      type="password"
-                      class="form-control"
-                      id="inputPassword4"
-                      placeholder="Password"
-                    />
-                  </div>
-                </div>
-                <div class="form-group">
-                  <label for="inputAddress">Address</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="inputAddress"
-                    placeholder="1234 Main St"
-                  />
-                </div>
-                <div class="form-group">
-                  <label for="inputAddress2">Address 2</label>
-                  <input
-                    type="text"
-                    class="form-control"
-                    id="inputAddress2"
-                    placeholder="Apartment, studio, or floor"
-                  />
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity" />
-                  </div>
-                  <div class="form-group col-md-4">
-                    <label for="inputState">State</label>
-                    <select id="inputState" class="form-control">
-                      <option selected>Choose...</option>
-                      <option>...</option>
-                    </select>
-                  </div>
-                  <div class="form-group col-md-2">
-                    <label for="inputZip">Zip</label>
-                    <input type="text" class="form-control" id="inputZip" />
-                  </div>
-                </div>
-                <div class="form-group">
-                  <div class="form-check">
-                    <input
-                      class="form-check-input"
-                      type="checkbox"
-                      id="gridCheck"
-                    />
-                    <label class="form-check-label" for="gridCheck">
-                      Check me out
-                    </label>
-                  </div>
-                </div>
-                <button type="submit" class="btn btn-primary">
-                  Sign in
-                </button>
-              </form>
+              <div className="col-3">
+                <label>Email </label>
+                <input className="form-control" type="email" />
+              </div>
+              <div className="col-3">
+                <label>Password </label>
+                <input className="form-control" type="password" />
+              </div>
+              <div className="col-3">
+                <label>First name </label>
+                <input className="form-control" type="text" />
+              </div>
+              <div className="col-3">
+                <label>Last name </label>
+                <input className="form-control" type="text" />
+              </div>
             </div>
           </div>
         </div>
