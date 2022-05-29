cf34ead351fb6a5aeb4bc4c52294dd266a2d596b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-29 22:12:52 +0900

    #57 Redux bonus 5.4

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -9,16 +9,19 @@ const initialState = {
 const adminReducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_GENDER_START:
+      console.log("Fire fetch gender start: ", action);
       return {
         ...state,
       };
 
     case actionTypes.FETCH_GENDER_SUCCESS:
+      console.log("Fire fetch gender success: ", action);
       return {
         ...state,
       };
 
     case actionTypes.FETCH_GENDER_FAILED:
+      console.log("Fire fetch gender failed: ", action);
       return {
         ...state,
       };
