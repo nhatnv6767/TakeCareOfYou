84167c309ad5ec35d86c73d323636cf56497a97f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-29 22:09:54 +0900

    #57 Redux bonus 5.3

--- src/containers/System/Admin/UserRedux.js
+++ src/containers/System/Admin/UserRedux.js
@@ -14,6 +14,8 @@ class UserRedux extends Component {
   }
 
   async componentDidMount() {
+    this.props.getGenderStart();
+
     // try {
     //   let res = await getAllCodeService("gender");
     //   if (res && res.errCode === 0) {
