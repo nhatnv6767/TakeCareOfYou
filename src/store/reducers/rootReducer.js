98e211b643304bc349c17f2e5071b7dc5fd7c913
nhatnv6767 <nhatnvse90183@gmail.com>
2022-05-29 21:57:25 +0900

    #57 Redux bonus 3

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -3,6 +3,7 @@ import { connectRouter } from "connected-react-router";
 
 import appReducer from "./appReducer";
 import userReducer from "./userReducer";
+import adminReducer from "./adminReducer";
 
 import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 import storage from "redux-persist/lib/storage";
