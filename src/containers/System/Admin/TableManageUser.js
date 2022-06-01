4299eda0e4a38ccf6d3b6aaf91b309e863464cfd
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-01 22:17:10 +0900

    #60 Get list users and delete user

---
+++ src/containers/System/Admin/TableManageUser.js
@@ -0,0 +1,214 @@
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { connect } from "react-redux";
+import "./UserManage.scss";
+import {
+  getAllUsers,
+  createNewUserService,
+  deleteUserService,
+  editUserService,
+} from "../../services/userService";
+import ModalUser from "./ModalUser";
+import { emitter } from "../../utils/emitter";
+import ModalEditUser from "./ModalEditUser";
+
+class UserManage extends Component {
+  constructor(props) {
+    super(props);
+    // khởi tạo những biến muốn dùng với thằng class này
+    this.state = {
+      arrUsers: [],
+      isOpenModalUser: false,
+      isOpenModalEditUser: false,
+      userEdit: {},
+    };
+  }
+
+  async componentDidMount() {
+    await this.getAllUsersFromReact();
+  }
+
+  getAllUsersFromReact = async () => {
+    let response = await getAllUsers("ALL");
+    if (response && response.errCode == 0) {
+      // hàm this.setState luôn là 1 hàm bất đồng bộ
+      this.setState(
+        {
+          arrUsers: response.users,
+        },
+        () => {
+          // tức là sau khi hoàn thành xong việc setState sẽ
+          // chạy ở đây, áp dụng với các trường hợp dữ liệu nhiều,
+        }
+      );
+    }
+  };
+
+  handleAddNewUser = () => {
+    this.setState({
+      isOpenModalUser: true,
+    });
+  };
+
+  toggleUserModal = () => {
+    this.setState({
+      isOpenModalUser: !this.state.isOpenModalUser,
+    });
+  };
+
+  toggleUserEditModal = () => {
+    this.setState({
+      isOpenModalEditUser: !this.state.isOpenModalEditUser,
+    });
+  };
+
+  createNewUser = async (data) => {
+    try {
+      let response = await createNewUserService(data);
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
+        await this.getAllUsersFromReact();
+        this.setState({
+          isOpenModalUser: false,
+        });
+        emitter.emit("EVENT_CLEAR_MODAL_DATA");
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  handleDeleteUser = async (user) => {
+    try {
+      let response = await deleteUserService(user.id);
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
+        await this.getAllUsersFromReact();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  handleEditUser = async (user) => {
+    this.setState({
+      isOpenModalEditUser: true,
+      userEdit: user,
+    });
+  };
+
+  doEditUser = async (user) => {
+    try {
+      let response = await editUserService(user);
+      if (response && response.errCode !== 0) {
+        alert(response.errMessage);
+      } else {
+        this.setState({
+          isOpenModalEditUser: false,
+        });
+        await this.getAllUsersFromReact();
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  /** Life cycle
+   *  Run component
+   * 1. Run constructor -> init state
+   * 2. Didmount ( set state)
+   * 3. Render
+   */
+
+  render() {
+    let arrUsers = this.state.arrUsers;
+    return (
+      <div className="users-container">
+        <ModalUser
+          isOpen={this.state.isOpenModalUser}
+          toggleFromParent={this.toggleUserModal}
+          // lưu ý nhỏ khi viết ở đây,
+          // không thêm dấu (), nếu thêm vào thì React mặc định
+          // sẽ gọi lúc vừa bắt đầu mount, và về cơ bản thì thằng cha
+          // không biết được thằng con sẽ truyền cái gì lên cho mình
+          // => không viết () ở đây
+          createNewUser={this.createNewUser}
+        />
+
+        {this.state.isOpenModalEditUser && (
+          <ModalEditUser
+            isOpen={this.state.isOpenModalEditUser}
+            toggleFromParent={this.toggleUserEditModal}
+            currentUser={this.state.userEdit}
+            editUser={this.doEditUser}
+          />
+        )}
+        <div className="title text-center">Manage users</div>
+        <div className="mx-1">
+          <button
+            className="btn btn-primary px-3"
+            onClick={() => this.handleAddNewUser()}
+          >
+            <i className="fas fa-plus"></i>
+            Add new user
+          </button>
+        </div>
+        <div className="users-table mt-3 mx-1">
+          <table id="customers">
+            <tbody>
+              <tr>
+                <th>Email</th>
+                <th>First name</th>
+                <th>Last name</th>
+                <th>Address</th>
+                <th>Actions</th>
+              </tr>
+
+              {arrUsers &&
+                arrUsers.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.email}</td>
+                      <td>{item.firstName}</td>
+                      <td>{item.lastName}</td>
+                      <td>{item.address}</td>
+                      <td>
+                        <button
+                          className="btn-edit"
+                          onClick={() => {
+                            this.handleEditUser(item);
+                          }}
+                        >
+                          <i className="fas fa-pencil-alt"></i>
+                        </button>
+                        <button
+                          className="btn-delete"
+                          onClick={() => {
+                            this.handleDeleteUser(item);
+                          }}
+                        >
+                          <i className="fas fa-trash"></i>
+                        </button>
+                      </td>
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserManage);
