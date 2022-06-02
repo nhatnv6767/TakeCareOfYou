e803ca3cdf348aeed1dba9cefa2207ad3f5a10bf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-02 22:16:12 +0900

    #61 CRUD with Redux 2.4

--- src/containers/System/Admin/TableManageUser.js
+++ src/containers/System/Admin/TableManageUser.js
@@ -30,6 +30,7 @@ class TableManageUser extends Component {
 
   handleEditUser = (user) => {
     console.log("handleEditUser: ", user);
+    this.props.handleEditUserFromParent("data from child");
   };
 
   render() {
