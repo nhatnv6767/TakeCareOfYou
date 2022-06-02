d64d9a5cf369f4d6feb1ee8aaa0eb4cbb59ec257
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-02 21:07:46 +0900

    #60 Get list users and delete user 8

--- src/containers/App.js
+++ src/containers/App.js
@@ -63,16 +63,15 @@ class App extends Component {
             </div>
 
             <ToastContainer
-              className="toast-container"
-              toastClassName="toast-item"
-              bodyClassName="toast-item-body"
-              autoClose={false}
-              hideProgressBar={true}
-              pauseOnHover={false}
-              pauseOnFocusLoss={true}
-              closeOnClick={false}
-              draggable={false}
-              closeButton={<CustomToastCloseButton />}
+              position="bottom-right"
+              autoClose={5000}
+              hideProgressBar={false}
+              newestOnTop={false}
+              closeOnClick
+              rtl={false}
+              pauseOnFocusLoss
+              draggable
+              pauseOnHover
             />
           </div>
         </Router>
