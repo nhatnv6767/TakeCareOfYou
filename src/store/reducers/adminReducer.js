7a61fc5eda9724b09750ade9dd38b77fec5b819b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-04 21:19:34 +0900

    #63 Load static doctors's infor 8

--- src/store/reducers/adminReducer.js
+++ src/store/reducers/adminReducer.js
@@ -6,6 +6,7 @@ const initialState = {
   roles: [],
   positions: [],
   users: [],
+  topDoctors: [],
 };
 
 const adminReducer = (state = initialState, action) => {
@@ -67,6 +68,23 @@ const adminReducer = (state = initialState, action) => {
 
     case actionTypes.FETCH_ALL_USERS_FAILED:
       state.users = [];
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_TOP_DOCTORS_START:
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_TOP_DOCTORS_SUCCESS:
+      state.topDoctors = action.dataDoctors;
+      return {
+        ...state,
+      };
+
+    case actionTypes.FETCH_TOP_DOCTORS_FAILED:
+      state.topDoctors = [];
       return {
         ...state,
       };
