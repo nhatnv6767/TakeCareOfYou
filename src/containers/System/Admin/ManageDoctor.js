6942f9243c6a3ac4311504b4218f58a13d9dccae
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-05 23:20:09 +0900

    #67 Add infor doctor 10

--- src/containers/System/Admin/ManageDoctor.js
+++ src/containers/System/Admin/ManageDoctor.js
@@ -37,6 +37,12 @@ class ManageDoctor extends Component {
         listDoctors: dataSelect,
       });
     }
+    if (prevProps.language !== this.props.language) {
+      let dataSelect = this.buildDataInputSelect(this.props.allDoctors);
+      this.setState({
+        listDoctors: dataSelect,
+      });
+    }
   }
   // Finish!
   handleEditorChange = ({ html, text }) => {
@@ -68,8 +74,8 @@ class ManageDoctor extends Component {
     if (inputData && inputData.length > 0) {
       inputData.map((item, index) => {
         let object = {};
-        let labelEn = `${item.lastName} ${item.firstName}`;
-        let labelVi = `${item.firstName} ${item.lastName}`;
+        let labelVi = `${item.lastName} ${item.firstName}`;
+        let labelEn = `${item.firstName} ${item.lastName}`;
         object.label = language === LANGUAGES.VI ? labelVi : labelEn;
         object.value = item.id;
         result.push(object);
