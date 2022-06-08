dd5c340e8fece485b0be406ea8fc10d773109935
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-09 01:05:24 +0900

    #69 Render doctor's page information 4.5

--- src/containers/HomePage/Section/OutStandingDoctor.js
+++ src/containers/HomePage/Section/OutStandingDoctor.js
@@ -1,111 +1,113 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { FormattedMessage } from "react-intl";
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {FormattedMessage} from "react-intl";
 import Slider from "react-slick";
 import * as actions from "../../../store/actions";
-import { LANGUAGES } from "../../../utils";
-import { withRouter } from "react-router";
+import {LANGUAGES} from "../../../utils";
+import {withRouter} from "react-router";
 
 class OutStandingDoctor extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      arrDoctors: [],
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
-      this.setState({
-        arrDoctors: this.props.topDoctorsRedux,
-      });
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDoctors: [],
+        };
     }
-  }
 
-  componentDidMount() {
-    this.props.loadTopDoctors();
-  }
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.topDoctorsRedux !== this.props.topDoctorsRedux) {
+            this.setState({
+                arrDoctors: this.props.topDoctorsRedux,
+            });
+        }
+    }
 
-  handleViewDetailDoctor = (doctor) => {
-    console.log("Check handleViewDetailDoctor: ", doctor);
-    this.props.history.push(`/detail-doctor/${doctor.id}`);
-  };
+    componentDidMount() {
+        this.props.loadTopDoctors();
+    }
 
-  render() {
-    let arrDoctors = this.state.arrDoctors;
-    let { language } = this.props;
-    // console.log("Check arrDoctors", arrDoctors);
-    return (
-      <div className="section-share section-outstanding-doctor">
-        <div className="section-container">
-          <div className="section-header">
+    handleViewDetailDoctor = (doctor) => {
+        console.log("Check handleViewDetailDoctor: ", doctor);
+        if (this.props.history) {
+            this.props.history.push(`/detail-doctor/${doctor.id}`);
+        }
+    };
+
+    render() {
+        let arrDoctors = this.state.arrDoctors;
+        let {language} = this.props;
+        // console.log("Check arrDoctors", arrDoctors);
+        return (
+            <div className="section-share section-outstanding-doctor">
+                <div className="section-container">
+                    <div className="section-header">
             <span className="title-section">
-              <FormattedMessage id="homepage.out-standing-doctor" />
+              <FormattedMessage id="homepage.out-standing-doctor"/>
             </span>
-            <button className="btn-section">
-              <FormattedMessage id="homepage.more-infor" />
-            </button>
-          </div>
-          <div className="section-body">
-            <Slider {...this.props.settings}>
-              {arrDoctors &&
-                arrDoctors.length > 0 &&
-                arrDoctors.map((item, index) => {
-                  let imageBase64 = "";
-                  if (item.image) {
-                    imageBase64 = new Buffer(item.image, "base64").toString(
-                      "binary"
-                    );
-                  }
-                  let nameVi = `${item.positionData.valueVi}, ${item.lastName} ${item.firstName}`;
-                  let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
-                  return (
-                    <div
-                      className="section-customize"
-                      key={index}
-                      onClick={() => this.handleViewDetailDoctor(item)}
-                    >
-                      <div className="customize-border">
-                        <div className="outer-bg">
-                          <div
-                            className="bg-image section-outstanding-doctor"
-                            style={{
-                              backgroundImage: `url(${imageBase64})`,
-                            }}
-                          />
-                        </div>
-                        <div className="position text-center">
-                          <div>
-                            {language === LANGUAGES.VI ? nameVi : nameEn}
-                          </div>
-                          <div>Nha khoa fix after</div>
-                        </div>
-                      </div>
+                        <button className="btn-section">
+                            <FormattedMessage id="homepage.more-infor"/>
+                        </button>
                     </div>
-                  );
-                })}
-            </Slider>
-          </div>
-        </div>
-      </div>
-    );
-  }
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+                            {arrDoctors &&
+                                arrDoctors.length > 0 &&
+                                arrDoctors.map((item, index) => {
+                                    let imageBase64 = "";
+                                    if (item.image) {
+                                        imageBase64 = new Buffer(item.image, "base64").toString(
+                                            "binary"
+                                        );
+                                    }
+                                    let nameVi = `${item.positionData.valueVi}, ${item.lastName} ${item.firstName}`;
+                                    let nameEn = `${item.positionData.valueEn}, ${item.firstName} ${item.lastName}`;
+                                    return (
+                                        <div
+                                            className="section-customize"
+                                            key={index}
+                                            onClick={() => this.handleViewDetailDoctor(item)}
+                                        >
+                                            <div className="customize-border">
+                                                <div className="outer-bg">
+                                                    <div
+                                                        className="bg-image section-outstanding-doctor"
+                                                        style={{
+                                                            backgroundImage: `url(${imageBase64})`,
+                                                        }}
+                                                    />
+                                                </div>
+                                                <div className="position text-center">
+                                                    <div>
+                                                        {language === LANGUAGES.VI ? nameVi : nameEn}
+                                                    </div>
+                                                    <div>Nha khoa fix after</div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    );
+                                })}
+                        </Slider>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.user.isLoggedIn,
-    topDoctorsRedux: state.admin.topDoctors,
-    language: state.app.language,
-  };
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        topDoctorsRedux: state.admin.topDoctors,
+        language: state.app.language,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    loadTopDoctors: () => dispatch(actions.fetchTopDoctorStart()),
-  };
+    return {
+        loadTopDoctors: () => dispatch(actions.fetchTopDoctorStart()),
+    };
 };
 
 export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor)
+    connect(mapStateToProps, mapDispatchToProps)(OutStandingDoctor)
 );
