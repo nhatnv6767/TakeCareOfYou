bc4edaa48a0ec9d701b9fd4a720fb09439af1a2e
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-09 00:48:55 +0900

    #69 Render doctor's page information 4.4

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -68,8 +68,7 @@ class DetailDoctor extends Component {
                     <div className="schedule-doctor"></div>
                     <div className="detail-infor-doctor">
                         {detailDoctor && detailDoctor.Markdown && detailDoctor.Markdown.contentHTML &&
-                            <div dangerouslySetInnerHTML={{__html: '<p>First &middot; Second</p>'}}>
-                                {detailDoctor.Markdown.contentHTML}
+                            <div dangerouslySetInnerHTML={{__html: detailDoctor.Markdown.contentHTML}}>
                             </div>
                         }
                     </div>
