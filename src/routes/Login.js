0bac1f27be32ce6f601a53c04b0f7db2d704f590
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-10 02:10:21 +0900

    #71 Role User when login 7.11

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { push } from "connected-react-router";
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {push} from "connected-react-router";
 
 import * as actions from "../store/actions";
-import { KeyCodeUtils, LanguageUtils } from "../utils";
+import {KeyCodeUtils, LanguageUtils} from "../utils";
 
 import userIcon from '../../src/assets/images/user.svg';
 import passIcon from '../../src/assets/images/pass.svg';
 import './Login.scss';
-import { FormattedMessage } from 'react-intl';
+import {FormattedMessage} from 'react-intl';
 
 import adminService from '../services/adminService';
 
@@ -22,7 +22,7 @@ class Login extends Component {
         username: '',
         password: '',
         loginError: ''
-    }
+    };
 
     state = {
         ...this.initialState
@@ -31,49 +31,49 @@ class Login extends Component {
     refresh = () => {
         this.setState({
             ...this.initialState
-        })
-    }
+        });
+    };
 
     onUsernameChange = (e) => {
-        this.setState({ username: e.target.value })
-    }
+        this.setState({username: e.target.value});
+    };
 
     onPasswordChange = (e) => {
-        this.setState({ password: e.target.value })
-    }
+        this.setState({password: e.target.value});
+    };
 
     redirectToSystemPage = () => {
-        const { navigate } = this.props;
+        const {navigate} = this.props;
         const redirectPath = '/system/user-manage';
         navigate(`${redirectPath}`);
-    }
+    };
 
     processLogin = () => {
-        const { username, password } = this.state;
+        const {username, password} = this.state;
 
-        const { adminLoginSuccess, adminLoginFail } = this.props;
+        const {adminLoginSuccess, adminLoginFail} = this.props;
         let loginBody = {
             username: 'admin',
             password: '123456'
-        }
+        };
         //sucess
         let adminInfo = {
             "tlid": "0",
             "tlfullname": "Administrator",
             "custype": "A",
             "accessToken": "eyJhbGciOiJIU"
-        }
+        };
 
         adminLoginSuccess(adminInfo);
         this.refresh();
         this.redirectToSystemPage();
         try {
-            adminService.login(loginBody)
+            adminService.login(loginBody);
         } catch (e) {
-            console.log('error login : ', e)
+            console.log('error login : ', e);
         }
 
-    }
+    };
 
     handlerKeyDown = (event) => {
         const keyCode = event.which || event.keyCode;
@@ -97,18 +97,18 @@ class Login extends Component {
     }
 
     render() {
-        const { username, password, loginError } = this.state;
-        const { lang } = this.props;
+        const {username, password, loginError} = this.state;
+        const {lang} = this.props;
 
         return (
             <div className="login-wrapper">
                 <div className="login-container">
                     <div className="form_login">
                         <h2 className="title">
-                            <FormattedMessage id="login.login" />
+                            <FormattedMessage id="login.login"/>
                         </h2>
                         <div className="form-group icon-true">
-                            <img className="icon" src={userIcon} alt="this" />
+                            <img className="icon" src={userIcon} alt="this"/>
                             <input
                                 placeholder={LanguageUtils.getMessageByKey("login.username", lang)}
                                 id="username"
@@ -121,7 +121,7 @@ class Login extends Component {
                         </div>
 
                         <div id="phone-input-container" className="form-group icon-true">
-                            <img className="icon" src={passIcon} alt="this" />
+                            <img className="icon" src={passIcon} alt="this"/>
                             <input
                                 placeholder={LanguageUtils.getMessageByKey("login.password", lang)}
                                 id="password"
@@ -134,8 +134,8 @@ class Login extends Component {
                         </div>
 
                         {loginError !== '' && (
-                            <div className='login-error'>
-                                <span className='login-error-message'>{loginError}</span>
+                            <div className="login-error">
+                                <span className="login-error-message">{loginError}</span>
                             </div>
                         )}
 
@@ -152,7 +152,7 @@ class Login extends Component {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
