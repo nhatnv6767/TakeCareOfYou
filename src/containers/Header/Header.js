a76620258cf6ae622d79784d6f8d99fede749a1b
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-10 01:14:42 +0900

    #71 Role User when login 5

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -7,76 +7,82 @@ import {adminMenu, doctorMenu} from "./menuApp";
 import "./Header.scss";
 import {LANGUAGES} from "../../utils";
 import {FormattedMessage} from "react-intl";
+import _ from "lodash";
 
 class Header extends Component {
-  handleChangeLanguage = (language) => {
-    // fire redux event : actions
-    this.props.changeLanguageAppRedux(language);
-  };
+    handleChangeLanguage = (language) => {
+        // fire redux event : actions
+        this.props.changeLanguageAppRedux(language);
+    };
 
-  componentDidMount() {
+    componentDidMount() {
+        let {userInfo} = this.props;
+        if (userInfo && !_.isEmpty(userInfo)) {
+            let role = userInfo.roleId;
+            
+        }
+        console.log("Userinfor: ", this.props.userInfo)
+    }
 
-  }
+    render() {
+        const {processLogout, language, userInfo} = this.props;
 
-  render() {
-    const {processLogout, language, userInfo} = this.props;
+        return (
+            <div className="header-container">
+                {/* thanh navigator */}
+                <div className="header-tabs-container">
+                    <Navigator menus={adminMenu}/>
+                </div>
 
-    return (
-        <div className="header-container">
-          {/* thanh navigator */}
-          <div className="header-tabs-container">
-            <Navigator menus={adminMenu}/>
-          </div>
-
-          <div className="languages">
+                <div className="languages">
           <span className="welcome">
             <FormattedMessage id="homeheader.welcome"/>,{" "}
-            {userInfo && userInfo.firstName ? userInfo.firstName : ""} !
+              {userInfo && userInfo.firstName ? userInfo.firstName : ""} !
           </span>
-            <span
-                className={
-                  language === LANGUAGES.VI ? "language-vi active" : "language-vi"
-                }
-                onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
-            >
+                    <span
+                        className={
+                            language === LANGUAGES.VI ? "language-vi active" : "language-vi"
+                        }
+                        onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}
+                    >
             VI
           </span>
-            <span
-                className={
-                  language === LANGUAGES.EN ? "language-en active" : "language-en"
-                }
-                onClick={() => this.handleChangeLanguage(LANGUAGES.EN)}
-            >
+                    <span
+                        className={
+                            language === LANGUAGES.EN ? "language-en active" : "language-en"
+                        }
+                        onClick={() => this.handleChangeLanguage(LANGUAGES.EN)}
+                    >
             EN
           </span>
-            {/* nút logout */}
-            <div
-                className="btn btn-logout"
-                onClick={processLogout}
-                title="Log out"
-            >
-              <i className="fas fa-sign-out-alt"></i>
+                    {/* nút logout */}
+                    <div
+                        className="btn btn-logout"
+                        onClick={processLogout}
+                        title="Log out"
+                    >
+                        <i className="fas fa-sign-out-alt"></i>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.user.isLoggedIn,
-    userInfo: state.user.userInfo,
-    language: state.app.language,
-  };
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        userInfo: state.user.userInfo,
+        language: state.app.language,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {
-    processLogout: () => dispatch(actions.processLogout()),
-    changeLanguageAppRedux: (language) =>
-        dispatch(actions.changeLanguageApp(language)),
-  };
+    return {
+        processLogout: () => dispatch(actions.processLogout()),
+        changeLanguageAppRedux: (language) =>
+            dispatch(actions.changeLanguageApp(language)),
+    };
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Header);
