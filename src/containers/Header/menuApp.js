8e200832fce86f3db593d40d2c27e7515b6799b3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-10 00:12:00 +0900

    #71 Role User when login 1

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -1,69 +1,107 @@
 export const adminMenu = [
-  {
-    //Quản lý người dùng
-    name: "menu.admin.manage-user",
-    menus: [
-      {
-        name: "menu.admin.crud",
-        link: "/system/user-manage",
-      },
+    {
+        //Quản lý người dùng
+        name: "menu.admin.manage-user",
+        menus: [
+            {
+                name: "menu.admin.crud",
+                link: "/system/user-manage",
+            },
 
-      {
-        name: "menu.admin.crud-redux",
-        link: "/system/user-redux",
-      },
-      {
-        name: "menu.admin.manage-doctor",
-        link: "/system/manage-doctor",
-        // subMenus: [
-        //   {
-        //     name: "menu.system.system-administrator.user-manage",
-        //     link: "/system/user-manage",
-        //   },
-        //   {
-        //     name: "menu.system.system-administrator.user-redux",
-        //     link: "/system/user-redux",
-        //   },
-        // ],
-      },
+            {
+                name: "menu.admin.crud-redux",
+                link: "/system/user-redux",
+            },
+            {
+                name: "menu.admin.manage-doctor",
+                link: "/system/manage-doctor",
+                // subMenus: [
+                //   {
+                //     name: "menu.system.system-administrator.user-manage",
+                //     link: "/system/user-manage",
+                //   },
+                //   {
+                //     name: "menu.system.system-administrator.user-redux",
+                //     link: "/system/user-redux",
+                //   },
+                // ],
+            },
 
-      {
-        name: "menu.admin.manage-admin",
-        link: "/system/user-admin",
-      },
-    ],
-  },
+            {
+                name: "menu.admin.manage-admin",
+                link: "/system/user-admin",
+            },
+        ],
+    },
 
-  {
-    //Quản lý phòng khám
-    name: "menu.admin.clinic",
-    menus: [
-      {
-        name: "menu.admin.manage-clinic",
-        link: "/system/manage-clinic",
-      },
-    ],
-  },
+    {
+        //Quản lý phòng khám
+        name: "menu.admin.clinic",
+        menus: [
+            {
+                name: "menu.admin.manage-clinic",
+                link: "/system/manage-clinic",
+            },
+        ],
+    },
 
-  {
-    //Quản lý chuyên khoa
-    name: "menu.admin.specialty",
-    menus: [
-      {
-        name: "menu.admin.manage-specialty",
-        link: "/system/manage-specialty",
-      },
-    ],
-  },
+    {
+        //Quản lý chuyên khoa
+        name: "menu.admin.specialty",
+        menus: [
+            {
+                name: "menu.admin.manage-specialty",
+                link: "/system/manage-specialty",
+            },
+        ],
+    },
 
-  {
-    //Quản lý cẩm nang
-    name: "menu.admin.handbook",
-    menus: [
-      {
-        name: "menu.admin.manage-handbook",
-        link: "/system/manage-handbook",
-      },
-    ],
-  },
+    {
+        //Quản lý cẩm nang
+        name: "menu.admin.handbook",
+        menus: [
+            {
+                name: "menu.admin.manage-handbook",
+                link: "/system/manage-handbook",
+            },
+        ],
+    },
+];
+
+
+export const doctorMenu = [
+    {
+        //Quản lý người dùng
+        name: "menu.admin.manage-user",
+        menus: [
+            {
+                name: "menu.admin.crud",
+                link: "/system/user-manage",
+            },
+
+            {
+                name: "menu.admin.crud-redux",
+                link: "/system/user-redux",
+            },
+            {
+                name: "menu.admin.manage-doctor",
+                link: "/system/manage-doctor",
+                // subMenus: [
+                //   {
+                //     name: "menu.system.system-administrator.user-manage",
+                //     link: "/system/user-manage",
+                //   },
+                //   {
+                //     name: "menu.system.system-administrator.user-redux",
+                //     link: "/system/user-redux",
+                //   },
+                // ],
+            },
+
+            {
+                name: "menu.admin.manage-admin",
+                link: "/system/user-admin",
+            },
+        ],
+    },
 ];
