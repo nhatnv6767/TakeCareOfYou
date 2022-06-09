282c1e5ca70d30be09c3b506da7779a088e6293c
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-10 01:44:50 +0900

    #71 Role User when login 7.5

--- src/routes/Doctor.js
+++ src/routes/Doctor.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Redirect, Route, Switch} from "react-router-dom";
 import Header from "../containers/Header/Header";
+import ManageSchedule from "../containers/System/Doctor/ManageSchedule";
 
 class Doctor extends Component {
     render() {
@@ -12,7 +13,7 @@ class Doctor extends Component {
                 <div className="system-container">
                     <div className="system-list">
                         <Switch>
-                            <Route path="/system/user-manage" component={UserManage}/>
+                            <Route path="/doctor/manage-schedule" component={ManageSchedule}/>
 
                         </Switch>
                     </div>
