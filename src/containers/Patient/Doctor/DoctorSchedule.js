86e19f423a78ca08a9711af585f3ca639d808dde
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-11 22:58:36 +0900

    #75 Create base component choose calendar 8

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -17,18 +17,15 @@ class DoctorSchedule extends Component {
     }
 
     render() {
-        let options = [
-            {label: 'Thứ 2', value: '2'},
-            {label: 'Thứ 3', value: '3'},
-            {label: 'Thứ 4', value: '4'},
-            {label: 'Thứ 5', value: '5'},
-        ];
         return (
             <div className="doctor-schedule-container">
                 <div className="all-schedules">
-                    <Select
-                        options={options}
-                    />
+                    <select>
+                        <option value="">Thứ 2</option>
+                        <option value="">Thứ 3</option>
+                        <option value="">Thứ 4</option>
+                        <option value="">Thứ 5</option>
+                    </select>
                 </div>
                 <div className="all-available-times">
 
