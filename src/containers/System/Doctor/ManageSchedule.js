930714718c3683f2950336bb1dbee8ac1385e9f9
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-11 14:59:33 +0900

    #72 Calendar create 3.17.1

--- src/containers/System/Doctor/ManageSchedule.js
+++ src/containers/System/Doctor/ManageSchedule.js
@@ -8,7 +8,6 @@ import {LANGUAGES, CRUD_ACTIONS} from "../../../utils";
 import {getDetailInforDoctorService} from "../../../services/userService";
 import DatePicker from "../../../components/Input/DatePicker";
 import moment from "moment";
-import FormattedDate from "../../../components/Formating/FormattedDate";
 
 class ManageSchedule extends Component {
     constructor(props) {
@@ -93,10 +92,11 @@ class ManageSchedule extends Component {
                             <DatePicker
                                 onChange={this.handleOnChangeDatePicker}
                                 className="form-control"
+                                value={new Date()}
                             />
                         </div>
                         <div className="col-12 pick-hour-container">
-                            <FormattedDate value={this.state.currentDate}/>
+
                         </div>
                         <button className="btn btn-primary">Lưu thông tin</button>
                     </div>
