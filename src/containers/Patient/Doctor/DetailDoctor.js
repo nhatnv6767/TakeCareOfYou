65af324333c07d16a85c3842eda2154592329ed7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-12 22:17:48 +0900

    #80 Design display infor detail doctor 14

--- src/containers/Patient/Doctor/DetailDoctor.js
+++ src/containers/Patient/Doctor/DetailDoctor.js
@@ -5,6 +5,7 @@ import "./DetailDoctor.scss";
 import {getDetailInforDoctorService} from "../../../services/userService";
 import {LANGUAGES} from "../../../utils";
 import DoctorSchedule from "./DoctorSchedule";
+import DoctorExtraInfor from "./DoctorExtraInfor";
 
 class DetailDoctor extends Component {
     constructor(props) {
@@ -84,7 +85,7 @@ class DetailDoctor extends Component {
                             />
                         </div>
                         <div className="content-right">
-
+                            <DoctorExtraInfor/>
                         </div>
                     </div>
                     <div className="detail-infor-doctor">
