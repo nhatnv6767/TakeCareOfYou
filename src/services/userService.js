eabb2088c3fe1e907a00456144cf2d0c954a1de3
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-14 00:34:29 +0900

    #83 Customize API to Load doctor's infor in Modal Booking 5

--- src/services/userService.js
+++ src/services/userService.js
@@ -58,6 +58,10 @@ const getExtraInforDoctorById = (doctorId) => {
     return axios.get(`/api/get-extra-infor-doctor-by-id?doctorId=${doctorId}`);
 };
 
+const getProfileDoctorById = (doctorId) => {
+    return axios.get(`/api/get-profile-doctor-by-id?doctorId=${doctorId}`);
+};
+
 export {
     handleLoginApi,
     getAllUsers,
