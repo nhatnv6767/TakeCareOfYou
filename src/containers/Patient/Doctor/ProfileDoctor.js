c085c2b136a4c0fd00e7d2942d737479cd1a1b2d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-14 00:07:26 +0900

    #83 Customize API to Load doctor's infor in Modal Booking 2

---
+++ src/containers/Patient/Doctor/ProfileDoctor.js
@@ -0,0 +1,41 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {FormattedMessage} from 'react-intl';
+import "./ProfileDoctor.scss";
+
+class ProfileDoctor extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {
+    }
+
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.language !== this.props.language) {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                DefaultClass
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileDoctor);
