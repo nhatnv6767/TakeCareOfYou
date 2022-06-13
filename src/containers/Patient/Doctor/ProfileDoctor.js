3064297ff2c545fd7ddb60bf5adba2b630bd70cc
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-14 00:54:57 +0900

    #83 Customize API to Load doctor's infor in Modal Booking 13

--- src/containers/Patient/Doctor/ProfileDoctor.js
+++ src/containers/Patient/Doctor/ProfileDoctor.js
@@ -14,19 +14,31 @@ class ProfileDoctor extends Component {
     }
 
     async componentDidMount() {
-        let id = this.props.doctorId;
+        let data = this.getInforDoctor(this.props.doctorId);
+        this.setState({
+            dataProfile: data,
+        });
+    }
 
+    getInforDoctor = async (id) => {
+        let result = {};
         if (id) {
             let res = await getProfileDoctorById(id);
-            console.log("Check getProfileDoctorById_res", res);
+            if (res && res.errCode === 0) {
+                result = res.data;
+            }
         }
-    }
+        return result;
+    };
 
 
     async componentDidUpdate(prevProps, prevState, snapshot) {
         if (prevProps.language !== this.props.language) {
 
         }
+        // if (prevProps.doctorId !== this.props.doctorId) {
+        //     this.getInforDoctor(this.props.doctorId);
+        // }
     }
 
     render() {
