be3e665d135080976d342e1b1604f1cbe0fe0bf7
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-17 21:55:54 +0900

    #90 Specialty func 2

---
+++ src/containers/System/Specialty/ManageSpecialty.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {FormattedMessage} from 'react-intl';
+
+class ManageSpecialty extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+    }
+
+    async componentDidMount() {
+    }
+
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.language !== this.props.language) {
+
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                ManageSpecialty
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ManageSpecialty);
