d1297a834b0f245b926b7fe5aff33c48ddf2d64d
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-18 00:32:01 +0900

    #90 Specialty func 10

--- src/containers/System/Specialty/ManageSpecialty.js
+++ src/containers/System/Specialty/ManageSpecialty.js
@@ -34,11 +34,11 @@ class ManageSpecialty extends Component {
                 <div className="add-new-specialty row">
                     <div className="col-6 form-group">
                         <label>Tên chuyên khoa</label>
-                        <input className="form-control"/>
+                        <input className="form-control" type="text"/>
                     </div>
                     <div className="col-6 form-group">
-                        <label>Tên chuyên khoa</label>
-                        <input className="form-control"/>
+                        <label>Ảnh chuyên khoa</label>
+                        <input className="form-control" type="file"/>
                     </div>
                     <div className="col-12">
                         <MdEditor
