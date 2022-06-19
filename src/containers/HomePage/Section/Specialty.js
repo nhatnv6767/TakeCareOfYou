b7d70bced2fecd27f1c8713176b2e31aa94a0aee
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-19 13:10:51 +0900

    #91 Specialty 9

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -30,8 +30,8 @@ class Specialty extends Component {
             <div className="section-share section-specialty">
                 <div className="section-container">
                     <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
-                        <button className="btn-section">Xem thêm</button>
+                        <span className="title-section"><FormattedMessage id="homepage.specialty"/></span>
+                        <button className="btn-section"><FormattedMessage id="homepage.more-infor"/></button>
                     </div>
                     <div className="section-body">
                         <Slider {...this.props.settings}>
