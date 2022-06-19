ea2b98b620d91bb4eaf7afbcf18c64017ce2fd33
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-19 12:49:39 +0900

    #91 Specialty 5

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -5,38 +5,57 @@ import Slider from "react-slick";
 import {getAllSpecialty} from "../../../services/userService";
 
 class Specialty extends Component {
-    render() {
-        return (
-            <div className="section-share section-specialty">
-                <div className="section-container">
-                    <div className="section-header">
-                        <span className="title-section">Chuyên khoa phổ biến</span>
-                        <button className="btn-section">Xem thêm</button>
-                    </div>
-                    <div className="section-body">
-                        <Slider {...this.props.settings}>
-                            <div className="section-customize">
-                                <div className="bg-image section-specialty"/>
-                                <div>Cơ xương khớp 1</div>
-                            </div>
-                            
-                        </Slider>
-                    </div>
-                </div>
-            </div>
-        );
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSpecialty: []
+    };
+  }
+
+  async componentDidMount() {
+    let res = await getAllSpecialty();
+
+    console.log("Specialty check response: ", res)
+    if (res && res.errCode === 0) {
+      this.setState({
+        dataSpecialty: res.data
+      })
     }
+  }
+
+  render() {
+    return (
+        <div className="section-share section-specialty">
+          <div className="section-container">
+            <div className="section-header">
+              <span className="title-section">Chuyên khoa phổ biến</span>
+              <button className="btn-section">Xem thêm</button>
+            </div>
+            <div className="section-body">
+              <Slider {...this.props.settings}>
+                <div className="section-customize">
+                  <div className="bg-image section-specialty"/>
+                  <div>Cơ xương khớp 1</div>
+                </div>
+
+              </Slider>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-        language: state.app.language,
-    };
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+    language: state.app.language,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
