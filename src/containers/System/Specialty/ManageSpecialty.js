93ea673884a11a3489aedc78eadff728a5f5ef18
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-19 12:18:56 +0900

    #91 Specialty

--- src/containers/System/Specialty/ManageSpecialty.js
+++ src/containers/System/Specialty/ManageSpecialty.js
@@ -63,6 +63,12 @@ class ManageSpecialty extends Component {
         let res = await createNewSpecialty(this.state);
         if (res && res.errCode === 0) {
             toast.success("Save new specialty succeed");
+            this.setState({
+                name: "",
+                imageBase64: "",
+                descriptionHTML: "",
+                descriptionMarkdown: "",
+            });
         } else {
             toast.error("Some error occurred while saving");
             console.log("handleSaveNewSpecialty check res: ", res);
