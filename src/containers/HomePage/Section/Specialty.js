a7903cb73047aa7212e6aa6121a07b8a2cda5852
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-19 12:54:00 +0900

    #91 Specialty 6

--- src/containers/HomePage/Section/Specialty.js
+++ src/containers/HomePage/Section/Specialty.js
@@ -6,56 +6,62 @@ import {getAllSpecialty} from "../../../services/userService";
 
 class Specialty extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      dataSpecialty: []
-    };
-  }
-
-  async componentDidMount() {
-    let res = await getAllSpecialty();
-
-    console.log("Specialty check response: ", res)
-    if (res && res.errCode === 0) {
-      this.setState({
-        dataSpecialty: res.data
-      })
+    constructor(props) {
+        super(props);
+        this.state = {
+            dataSpecialty: []
+        };
     }
-  }
 
-  render() {
-    return (
-        <div className="section-share section-specialty">
-          <div className="section-container">
-            <div className="section-header">
-              <span className="title-section">Chuyên khoa phổ biến</span>
-              <button className="btn-section">Xem thêm</button>
-            </div>
-            <div className="section-body">
-              <Slider {...this.props.settings}>
-                <div className="section-customize">
-                  <div className="bg-image section-specialty"/>
-                  <div>Cơ xương khớp 1</div>
+    async componentDidMount() {
+        let res = await getAllSpecialty();
+
+        console.log("Specialty check response: ", res);
+        if (res && res.errCode === 0) {
+            this.setState({
+                dataSpecialty: res.data ? res.data : []
+            });
+        }
+    }
+
+    render() {
+        let {dataSpecialty} = this.state;
+        return (
+            <div className="section-share section-specialty">
+                <div className="section-container">
+                    <div className="section-header">
+                        <span className="title-section">Chuyên khoa phổ biến</span>
+                        <button className="btn-section">Xem thêm</button>
+                    </div>
+                    <div className="section-body">
+                        <Slider {...this.props.settings}>
+                            {dataSpecialty && dataSpecialty.length > 0 &&
+                                dataSpecialty.map((item, index) => {
+                                    return (
+                                        <div className="section-customize" key={index}>
+                                            <div className="bg-image section-specialty"/>
+                                            <div>{item.name}</div>
+                                        </div>
+                                    );
+                                })
+                            }
+                        </Slider>
+                    </div>
                 </div>
-
-              </Slider>
             </div>
-          </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    isLoggedIn: state.user.isLoggedIn,
-    language: state.app.language,
-  };
+    return {
+        isLoggedIn: state.user.isLoggedIn,
+        language: state.app.language,
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
-  return {};
+    return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Specialty);
