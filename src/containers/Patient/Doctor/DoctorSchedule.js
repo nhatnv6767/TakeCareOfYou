37a0751e1d74d441d191cef762c64809b30cf79a
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-22 22:03:44 +0900

    #95 Done Load static detail Specialty 12

--- src/containers/Patient/Doctor/DoctorSchedule.js
+++ src/containers/Patient/Doctor/DoctorSchedule.js
@@ -22,6 +22,14 @@ class DoctorSchedule extends Component {
     async componentDidMount() {
         let {language} = this.props;
         let allDays = this.getArrDays(language);
+
+        if (this.props.doctorIdFromParent) {
+            let res = await getScheduleDoctorByDate(this.props.doctorIdFromParent, allDays[0].value);
+            this.setState({
+                allAvailableTimes: res.data ? res.data : []
+            });
+        }
+
         if (allDays && allDays.length > 0) {
             this.setState({
                 allDays: allDays,
