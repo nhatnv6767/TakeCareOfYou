e6010a97fd17d2f05e3a2e8cc01d72fbded6a778
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 22:05:07 +0900

    #97 Detail Clinic 23

--- src/store/actions/adminActions.js
+++ src/store/actions/adminActions.js
@@ -9,6 +9,7 @@ import {
     getAllDoctorsService,
     saveDetailDoctorService,
     getAllSpecialty,
+    getAllClinic,
 } from "../../services/userService";
 
 import {toast} from "react-toastify";
@@ -402,17 +403,20 @@ export const fetchRequiredDoctorInforStart = () => {
             let resPayment = await getAllCodeService("PAYMENT");
             let resProvince = await getAllCodeService("PROVINCE");
             let resSpecialty = await getAllSpecialty();
+            let resClinic = await getAllClinic()
             if (
                 resPrice && resPrice.errCode === 0 &&
                 resPayment && resPayment.errCode === 0 &&
                 resProvince && resProvince.errCode === 0 &&
-                resSpecialty && resSpecialty.errCode === 0
+                resSpecialty && resSpecialty.errCode === 0 &&
+                resClinic && resClinic.errCode === 0
             ) {
                 let data = {
                     resPrice: resPrice.data,
                     resPayment: resPayment.data,
                     resProvince: resProvince.data,
-                    resSpecialty: resSpecialty.data
+                    resSpecialty: resSpecialty.data,
+                    resClinic: resClinic.data
                 };
                 dispatch(fetchRequiredDoctorInforSuccess(data));
             } else {
