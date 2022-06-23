099058ac207c82f8b15208fa167f8338c21057f0
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 21:12:27 +0900

    #97 Detail Clinic 5

--- src/containers/HomePage/Section/MedicalFacility.js
+++ src/containers/HomePage/Section/MedicalFacility.js
@@ -6,68 +6,74 @@ import {getAllClinic} from "../../../services/userService";
 
 class MedicalFacility extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataClinics: [],
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataClinics: [],
+    };
+  }
 
-    async componentDidMount() {
-        let res = await getAllClinic();
-        console.log("Check res MedicalFacility", res);
+  async componentDidMount() {
+    let res = await getAllClinic();
+    if (res && res.errCode === 0) {
+      this.setState({
+        dataClinics: res.data ? res.data : []
+      });
     }
+  }
 
-    render() {
-        return (
-            <div className="section-share section-medical-facility">
-                <div className="section-container">
-                    <div className="section-header">
-                        <span className="title-section">Cơ sở y tế nổi bật</span>
-                        <button className="btn-section">Xem thêm</button>
-                    </div>
-                    <div className="section-body">
-                        <Slider {...this.props.settings}>
-                            <div className="section-customize">
-                                <div className="bg-image section-medical-facility"/>
-                                <div>Hệ thống Y tế Thu Cúc 1</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-medical-facility"/>
-                                <div>Hệ thống Y tế Thu Cúc 2</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-medical-facility"/>
-                                <div>Hệ thống Y tế Thu Cúc 3</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-medical-facility"/>
-                                <div>Hệ thống Y tế Thu Cúc 4</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-medical-facility"/>
-                                <div>Hệ thống Y tế Thu Cúc 5</div>
-                            </div>
-                            <div className="section-customize">
-                                <div className="bg-image section-medical-facility"/>
-                                <div>Hệ thống Y tế Thu Cúc 6</div>
-                            </div>
-                        </Slider>
-                    </div>
-                </div>
+  render() {
+
+    let {dataClinics} = this.state;
+    return (
+        <div className="section-share section-medical-facility">
+          <div className="section-container">
+            <div className="section-header">
+              <span className="title-section">Cơ sở y tế nổi bật</span>
+              <button className="btn-section">Xem thêm</button>
             </div>
-        );
-    }
+            <div className="section-body">
+              <Slider {...this.props.settings}>
+                <div className="section-customize">
+                  <div className="bg-image section-medical-facility"/>
+                  <div>Hệ thống Y tế Thu Cúc 1</div>
+                </div>
+                <div className="section-customize">
+                  <div className="bg-image section-medical-facility"/>
+                  <div>Hệ thống Y tế Thu Cúc 2</div>
+                </div>
+                <div className="section-customize">
+                  <div className="bg-image section-medical-facility"/>
+                  <div>Hệ thống Y tế Thu Cúc 3</div>
+                </div>
+                <div className="section-customize">
+                  <div className="bg-image section-medical-facility"/>
+                  <div>Hệ thống Y tế Thu Cúc 4</div>
+                </div>
+                <div className="section-customize">
+                  <div className="bg-image section-medical-facility"/>
+                  <div>Hệ thống Y tế Thu Cúc 5</div>
+                </div>
+                <div className="section-customize">
+                  <div className="bg-image section-medical-facility"/>
+                  <div>Hệ thống Y tế Thu Cúc 6</div>
+                </div>
+              </Slider>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-    };
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MedicalFacility);
