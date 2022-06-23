4d50c8e56a39b82ccdfdc4ba3eec41dad197e872
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 22:51:40 +0900

    #98 Medical appointment list management interface 1

--- src/containers/Header/menuApp.js
+++ src/containers/Header/menuApp.js
@@ -84,6 +84,11 @@ export const doctorMenu = [
                 name: "menu.doctor.manage-schedule",
                 link: "/doctor/manage-schedule",
             },
+            {
+                //Quản lý bệnh nhân khám bệnh của bác sĩ
+                name: "menu.doctor.manage-schedule",
+                link: "/doctor/manage-schedule",
+            },
         ]
     }
 
