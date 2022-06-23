2d077d6ad82fe1455bfcaaa91473e31702ff91cf
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 21:32:58 +0900

    #97 Detail Clinic 14

---
+++ src/containers/Patient/Clinic/DetailClinic.js
@@ -0,0 +1,192 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import {FormattedMessage} from 'react-intl';
+import "./DetailClinic.scss";
+import HomeHeader from "../../HomePage/HomeHeader";
+import DoctorSchedule from "../Doctor/DoctorSchedule";
+import DoctorExtraInfor from "../Doctor/DoctorExtraInfor";
+import ProfileDoctor from "../Doctor/ProfileDoctor";
+import {getDetailSpecialtyById, getAllCodeService} from "../../../services/userService";
+import _ from "lodash";
+import {LANGUAGES} from "../../../utils";
+
+class DetailClinic extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            arrDoctorId: [],
+            dataDetailSpecialty: {},
+            listProvince: [],
+        };
+    }
+
+    async componentDidMount() {
+        if (
+            this.props.match &&
+            this.props.match.params &&
+            this.props.match.params.id
+        ) {
+            let id = this.props.match.params.id;
+
+            let res = await getDetailSpecialtyById({
+                id: id,
+                location: "ALL"
+            });
+
+            let resProvince = await getAllCodeService("PROVINCE");
+
+            if (res && res.errCode === 0 && resProvince.errCode === 0) {
+                let data = res.data;
+                let arrDoctorId = [];
+                if (data && !_.isEmpty(data)) {
+                    let arr = data.doctorSpecialty;
+                    if (arr && arr.length > 0) {
+                        arr.map((item) => {
+                            arrDoctorId.push(item.doctorId);
+                        });
+                    }
+                }
+
+                let dataProvince = resProvince.data;
+                if (dataProvince && dataProvince.length > 0) {
+                    dataProvince.unshift({
+                        keyMap: "ALL",
+                        type: "PROVINCE",
+                        valueEn: "All",
+                        valueVi: "Toàn quốc",
+                    });
+                }
+                this.setState({
+                    dataDetailSpecialty: res.data,
+                    arrDoctorId: arrDoctorId,
+                    listProvince: dataProvince ? dataProvince : []
+                });
+            }
+        }
+    }
+
+
+    async componentDidUpdate(prevProps, prevState, snapshot) {
+        if (prevProps.language !== this.props.language) {
+
+        }
+    }
+
+    handleOnChangeSelect = async (event) => {
+        if (
+            this.props.match &&
+            this.props.match.params &&
+            this.props.match.params.id
+        ) {
+            let id = this.props.match.params.id;
+            let location = event.target.value;
+
+
+            let res = await getDetailSpecialtyById({
+                id: id,
+                location: location,
+            });
+
+            if (res && res.errCode === 0) {
+                let data = res.data;
+                let arrDoctorId = [];
+                if (data && !_.isEmpty(data)) {
+                    let arr = data.doctorSpecialty;
+                    if (arr && arr.length > 0) {
+                        arr.map((item) => {
+                            arrDoctorId.push(item.doctorId);
+                        });
+                    }
+                }
+
+                this.setState({
+                    dataDetailSpecialty: res.data,
+                    arrDoctorId: arrDoctorId,
+                });
+            }
+        }
+    };
+
+    render() {
+        let {arrDoctorId, dataDetailSpecialty, listProvince} = this.state;
+        console.log("DetailSpecialty Check state: ", this.state);
+        let {language} = this.props;
+        return (
+            <div className="detail-specialty-container">
+                <HomeHeader isShowBanner={false}/>
+                <div className="detail-specialty-body">
+                    <div className="description-specialty">
+                        {dataDetailSpecialty && !_.isEmpty(dataDetailSpecialty)
+
+                            &&
+                            <div dangerouslySetInnerHTML={{__html: dataDetailSpecialty.descriptionHTML}}>
+                            </div>
+
+                        }
+                    </div>
+
+                    <div className="search-sp-doctor">
+                        <select onChange={(event) => this.handleOnChangeSelect(event)}>
+                            {listProvince && listProvince.length > 0 &&
+                                listProvince.map((item, index) => {
+                                    return (
+                                        <option key={index} value={item.keyMap}>
+                                            {language === LANGUAGES.VI ? item.valueVi : item.valueEn}
+                                        </option>
+                                    );
+                                })
+                            }
+
+                        </select>
+                    </div>
+                    {arrDoctorId && arrDoctorId.length > 0 &&
+                        arrDoctorId.map((item, index) => {
+                            return (
+                                <div className="each-doctor" key={index}>
+                                    <div className="dt-content-left">
+                                        <div className="profile-doctor">
+                                            <ProfileDoctor
+                                                doctorId={item}
+                                                isShowDescriptionDoctor={true}
+                                                isShowLinkDetail={true}
+                                                isShowPrice={false}
+                                                // dataTime={dataTime}
+                                            />
+                                        </div>
+                                    </div>
+
+                                    <div className="dt-content-right">
+                                        <div className="doctor-schedule">
+                                            <DoctorSchedule
+                                                doctorIdFromParent={item}
+                                            />
+                                        </div>
+
+                                        <div className="doctor-extra-infor">
+                                            <DoctorExtraInfor
+                                                doctorIdFromParent={item}
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            );
+                        })
+                    }
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        language: state.app.language,
+    };
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(DetailClinic);
