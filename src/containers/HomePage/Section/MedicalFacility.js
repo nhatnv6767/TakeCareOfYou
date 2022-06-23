f88909808d32c9c1a70b2765f0846f4e5dbe8df2
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 21:17:10 +0900

    #97 Detail Clinic 8

--- src/containers/HomePage/Section/MedicalFacility.js
+++ src/containers/HomePage/Section/MedicalFacility.js
@@ -6,62 +6,67 @@ import {getAllClinic} from "../../../services/userService";
 
 class MedicalFacility extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            dataClinics: [],
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataClinics: [],
+    };
+  }
+
+  async componentDidMount() {
+    let res = await getAllClinic();
+    if (res && res.errCode === 0) {
+      this.setState({
+        dataClinics: res.data ? res.data : []
+      });
     }
+  }
 
-    async componentDidMount() {
-        let res = await getAllClinic();
-        if (res && res.errCode === 0) {
-            this.setState({
-                dataClinics: res.data ? res.data : []
-            });
-        }
-    }
+  render() {
 
-    render() {
-
-        let {dataClinics} = this.state;
-        return (
-            <div className="section-share section-medical-facility">
-                <div className="section-container">
-                    <div className="section-header">
-                        <span className="title-section">Cơ sở y tế nổi bật</span>
-                        <button className="btn-section">Xem thêm</button>
-                    </div>
-                    <div className="section-body">
-                        <Slider {...this.props.settings}>
-                            {dataClinics && dataClinics.length > 0 &&
-                                dataClinics.map((item, index) => {
-                                    return (
-                                        <div className="section-customize">
-                                            <div className="bg-image section-medical-facility"/>
-                                            <div>Hệ thống Y tế Thu Cúc 1</div>
-                                        </div>
-                                    );
-                                })
-                            }
-
-
-                        </Slider>
-                    </div>
-                </div>
+    let {dataClinics} = this.state;
+    return (
+        <div className="section-share section-medical-facility">
+          <div className="section-container">
+            <div className="section-header">
+              <span className="title-section">Cơ sở y tế nổi bật</span>
+              <button className="btn-section">Xem thêm</button>
             </div>
-        );
-    }
+            <div className="section-body">
+              <Slider {...this.props.settings}>
+                {dataClinics && dataClinics.length > 0 &&
+                    dataClinics.map((item, index) => {
+                      return (
+                          <div className="section-customize" key={index}>
+                            <div
+                                className="bg-image section-medical-facility"
+                                style={{
+                                  backgroundImage: `url(${item.image})`,
+                                }}
+                            />
+                            <div>{item.name}</div>
+                          </div>
+                      );
+                    })
+                }
+
+
+              </Slider>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
-    return {
-        isLoggedIn: state.user.isLoggedIn,
-    };
+  return {
+    isLoggedIn: state.user.isLoggedIn,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(MedicalFacility);
