869f284862fc0c260939f13218e2a61fb9c0ea21
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-23 21:36:37 +0900

    #97 Detail Clinic 16

--- src/containers/App.js
+++ src/containers/App.js
@@ -64,6 +64,7 @@ class App extends Component {
                                     <Route path={path.HOMEPAGE} component={HomePage}/>
                                     <Route path={path.DETAIL_DOCTOR} component={DetailDoctor}/>
                                     <Route path={path.DETAIL_SPECIALTY} component={DetailSpecialty}/>
+                                    <Route path={path.DETAIL_CLINIC} component={DetailClinic}/>
                                     <Route path={path.VERIFY_EMAIL_BOOKING} component={VerifyEmail}/>
                                 </Switch>
                             </CustomScrollbars>
