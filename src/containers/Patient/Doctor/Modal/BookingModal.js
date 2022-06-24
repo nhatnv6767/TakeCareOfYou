4a38ed134e93ed44d3f78af75a6dd17e761fd44f
nhatnv6767 <nhatnvse90183@gmail.com>
2022-06-25 07:21:47 +0900

    #99 Show list patients for doctor 12

--- src/containers/Patient/Doctor/Modal/BookingModal.js
+++ src/containers/Patient/Doctor/Modal/BookingModal.js
@@ -134,17 +134,13 @@ class BookingModal extends Component {
         let timeString = this.buildTimeBooking(this.props.dataTime);
         let doctorName = this.buildDoctorName(this.props.dataTime);
 
-        console.log("Booking Modal : ", this.props.dataTime)
-
-        return;
-
         let res = await postPatientBookAppointment({
             fullName: this.state.fullName,
             phoneNumber: this.state.phoneNumber,
             email: this.state.email,
             address: this.state.address,
             reason: this.state.reason,
-            date: date,
+            date: this.props.dataTime.date,
             birthday: date,
             selectedGender: this.state.selectedGender.value,
             doctorId: this.state.doctorId,
